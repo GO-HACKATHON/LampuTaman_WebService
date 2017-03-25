93d0dba3eb156f2ac2932321e808ee2a64b37b4e
irfanhanif <if.irfanhanif@gmail.com>
2017-03-26 01:10:38 +0700

    Solve merge in MapController

--- app/Http/Controllers/Api/MapController.php
+++ app/Http/Controllers/Api/MapController.php
@@ -27,7 +27,6 @@ class MapController extends Controller {
         $waktu = date("Y-m-d H:i:s");
         $waktu = null;
         $kantor_polisi = $this->getDistance($lat_daerah, $lng_daerah, -6.244508, 106.800628);
-
         try {
             $insertPin = DB::table('daerahrawan')->insert([
                 'email_pengguna' => $email_pengguna,
@@ -47,8 +46,8 @@ class MapController extends Controller {
         $pengguna = new PenggunaController;
         return $pengguna->httpResponse('200', 'Penandaan daerah berhasil.');
     }
-    
-    
+
+
     /**
     * Fungsi untuk mendapatkan status keamanan
     * @param \Illuminate\Http\Request $request
@@ -58,22 +57,17 @@ class MapController extends Controller {
         $latNow = $request->lat;
         $lngNow = $request->lng;
         $sumDistance = 0;
-        //$dump = array();
-        
+
         $allLoc = DB::table('daerahrawan')->select('lat_daerah','lng_daerah')->get();
         foreach ($allLoc as $row){
             $distance = $this->getDistance($latNow, $lngNow, $row->lat_daerah, $row->lng_daerah);
-            array_push($dump, $distance);
             if($distance < 50) $sumDistance++;
-            
         }
-        
-        //return $dump;
         if ($sumDistance <= 5) return "aman";
         else if ($sumDistance > 5 && $sumDistance <= 10) return "sedang";
         else if ($sumDistance > 10) return "rawan";
     }
-    
+
     /**
     * Fungsi untuk mencari jarak antara dua koordinat latitude dan longitude
     * @param logitude lokasi 1
@@ -82,20 +76,20 @@ class MapController extends Controller {
     * @param latitude lokasi 2
     * @return jarak antara kedua koordinat
     */
-    public function getDistance($lat1, $lng1, $lat2, $lng2){ 
-        $r= 6371000; 
-        $currLat1 = deg2rad($lat1); 
-        $currLat2 = deg2rad($lat2); 
-        $deltaLat = deg2rad($lat2-$lat1); 
-        $deltaLng = deg2rad($lng2-$lng1); 
- 
-        $a = (sin($deltaLat/2) * sin($deltaLat/2)) + 
+    public function getDistance($lat1, $lng1, $lat2, $lng2){
+        $r= 6371000;
+        $currLat1 = deg2rad($lat1);
+        $currLat2 = deg2rad($lat2);
+        $deltaLat = deg2rad($lat2-$lat1);
+        $deltaLng = deg2rad($lng2-$lng1);
+
+        $a = (sin($deltaLat/2) * sin($deltaLat/2)) +
                 (cos($currLat1) * cos($currLat2) * sin($deltaLng) * sin($deltaLng));
         $c = 2 * atan2(sqrt($a), sqrt(1-$a));
         $d = $r * $c;
- 
-        return $d; 
+
+        return $d;
     }
-    
-    
+
+
 }
